d859763cc2163952a17484f534098666ff16ea39
AliGhaffarian <alighaffarian9@gmail.com>
2024-09-15 13:00:42 +0330

    better file structure

--- rev/flag_injection/helpers/hexgenerator.py
+++
@@ -1,9 +0,0 @@
-
-register_set = ["rax", "rcx", "rdx", "rbx", "rsi", "rdi", "rsp", "rbp", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"]
-for register in register_set:
-    string = input()
-    string=string.replace(".","1")
-    print(register +"\t" + hex(int(string, 2)))
-
-
-
